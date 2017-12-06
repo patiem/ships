fac5d5fa2dd60ff2975511097869378b6e4d8021
madzia <magdaalidia@gmail.com>
2017-12-06 22:04:12 +0100

    simple start gui

--- client/src/main/java/com/epam/ships/client/Main.java
+++ client/src/main/java/com/epam/ships/client/Main.java
@@ -9,21 +9,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-   // private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setMinHeight(275);
-        primaryStage.setMinWidth(300);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(600);
 
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        //logger.info("Client is up and running.");
+        logger.info("Client is up and running.");
         launch(args);
     }
 }
