d284843218070463cbee61b3deeefa0f96785f26
Sandor Korotkevics <korotkevics.sandor@gmail.com>
2017-12-06 14:56:44 +0100

    Add server and client modules

---
+++ server/src/test/java/com/epam/ships/server/DummyTest.java
@@ -0,0 +1,13 @@
+package com.epam.ships.server;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class DummyTest {
+   
+   @Test
+   public void testDummy() {
+       assertTrue(true);
+   }
+}
