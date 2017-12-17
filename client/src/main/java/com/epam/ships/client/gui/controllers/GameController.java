d159394854eb75803c0a1e455972ac942cf5e6be
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2017-12-17 21:24:26 +0100

    gui enable shoting

--- client/src/main/java/com/epam/ships/client/gui/controllers/GameController.java
+++ client/src/main/java/com/epam/ships/client/gui/controllers/GameController.java
@@ -1,11 +1,53 @@
 package com.epam.ships.client.gui.controllers;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.HPos;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class GameController {
 
     @FXML
+    private GridPane yourBoard;
+
+    @FXML
+    private GridPane opponentBoard;
+
+    @FXML
     public void initialize() {
+        NumberBinding yourBoardRectsAreaSize = Bindings.min(yourBoard.heightProperty(), yourBoard.widthProperty().add(-50));
+        NumberBinding yourBoardRectsAreaSizeH = Bindings.min(yourBoard.heightProperty(), yourBoard.widthProperty()).add(-50);
+
+        NumberBinding opponentBoardRectsAreaSize = Bindings.min(yourBoard.heightProperty(), yourBoard.widthProperty().add(-50));
+        NumberBinding opponentBoardRectsAreaSizeH = Bindings.min(yourBoard.heightProperty(), yourBoard.widthProperty()).add(-50);
+
+        int boardSize = 10;
+
+        for(int i = 0; i < boardSize; i++ ) {
+            for(int j = 0; j < boardSize; j++) {
+
+                Rectangle yourRect = new Rectangle(15, 15, Color.GRAY);
+                Rectangle opponentRect = new Rectangle(15, 15, Color.GRAY);
+
+                opponentRect.setOnMouseClicked(mouseEvent -> opponentRect.setFill(Color.BLACK));
+
+                yourRect.widthProperty().bind(yourBoardRectsAreaSize.divide(boardSize));
+                yourRect.heightProperty().bind(yourBoardRectsAreaSizeH.divide(boardSize));
+
+                opponentRect.widthProperty().bind(opponentBoardRectsAreaSize.divide(boardSize));
+                opponentRect.heightProperty().bind(opponentBoardRectsAreaSizeH.divide(boardSize));
+
+                yourBoard.add(yourRect, i, j);
+                opponentBoard.add(opponentRect, i, j);
+
+                GridPane.setHalignment(yourRect, HPos.CENTER);
+                GridPane.setHalignment(opponentRect, HPos.CENTER);
+            }
+        }
     }
 }
