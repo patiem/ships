64a4a1641085426bdf74f1b748019f4e25a3d8c4
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2017-12-17 08:36:41 +0100

    add param tests and repair some issues

--- client/src/test/java/com/epam/ships/client/validators/PortValidatorTest.java
+++ client/src/test/java/com/epam/ships/client/validators/PortValidatorTest.java
@@ -1,46 +1,56 @@
 package com.epam.ships.client.validators;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class PortValidatorTest {
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void shouldThrowIllegalArgumentExceptionWhenTryToConvertNotANumber() {
+
+    @DataProvider(name = "properValues")
+    public static Object[][] properData() {
+        return new Object[][] {
+                {"1", 1},
+                {"1234", 1234},
+                {"8189", 8189},
+                {"65535", 65535}
+        };
+    }
+
+    @DataProvider(name = "invalidValues")
+    public static Object[][] invalidData() {
+        return new Object[][] {
+                {"-1"},
+                {"0"},
+                {""},
+                {"66666"},
+                {"ala"}
+        };
+    }
+
+
+    @Test(expectedExceptions = IllegalArgumentException.class, dataProvider = "invalidValues")
+    public void shouldThrowIllegalArgumentException(String testPort) {
         //given
         PortValidator portValidator = new PortValidator();
-        String testObject = "ala";
 
         //when
-        portValidator.asInt(testObject);
+        portValidator.asInt(testPort);
         //then
         //throws
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void shouldThrowIllegalArgumentExceptionWhenTryToPassOutOfBoundValue() {
+    @Test(dataProvider = "properValues")
+    public void shouldGetProperValueOfPort(String testPort, int properReturnedPort) {
         //given
         PortValidator portValidator = new PortValidator();
-        String testObject = "666666";
 
         //when
-        portValidator.asInt(testObject);
-        //then
-        //throws
-    }
-
-    @Test
-    public void shouldGetProperValueOfPort() {
-        //given
-        PortValidator portValidator = new PortValidator();
-        String testObject = "6666";
-
-        //when
-        int port = portValidator.asInt(testObject);
+        int port = portValidator.asInt(testPort);
 
         //then
-        assertEquals(port, 6666);
+        assertEquals(port, properReturnedPort);
     }
 
 }
