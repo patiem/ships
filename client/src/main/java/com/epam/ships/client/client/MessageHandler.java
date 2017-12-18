0de5c04903ff7dba3e75f3e38e1f8a6e91678068
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2017-12-18 21:05:39 +0100

    remove trash

--- client/src/main/java/com/epam/ships/client/client/MessageHandler.java
+++ client/src/main/java/com/epam/ships/client/client/MessageHandler.java
@@ -19,7 +19,7 @@ class MessageHandler {
         this.triggers = new HashMap<>();
         this.triggers.put("opponentConnected", new OpponentConnectedTrigger());
         this.triggers.put("shot", new OpponentShotTrigger());
-        this.triggers.put("Connection", new ConnectionEndTigger());8
+        this.triggers.put("Connection", new ConnectionEndTigger());
 
         //TODO: react to end!
     }
