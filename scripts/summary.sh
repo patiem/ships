83dc0ffda2d26448d6e79ec32b068111af3d046b
Sandor Korotkevics <sandor_korotkevics@epam.com>
2017-12-18 14:51:19 +0100

    Improve summary.sh

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-echo ""
-echo "<<< AUTO SUMMARY >>>"
-echo ""
+# Author: Sandor
+# Since: 2017-12-18
+#
+# [Important]
+#
+#   1) Make sure the branch you're on has no uncommitted changes.
+#      Auto-commit wasn't introduced for safety reasons.
+#   2) The assumption is that the script is placed within ./scripts/
+#      directory.
+#
+# [Description]
+#
+# It gives a quick summary of what a project has.
+# Most of output is automated, however some values
+# do have to be pre-defined manually.
+#
 
-# Go to the root directory and switch to master branch quietly
+
+#Go to the root directory and switch to master branch quietly.
+#Won't work properly if there are uncommitted changes (see above).
 cd ..
 git checkout master --quiet
+
+#[] Auto output
 
 echo "[ Q ] How many tests do we have.."
 mvn test | grep -o "Tests run: [0-9]" | sed 's/[^0-9]*//g' | awk '{ SUM += $1} END { print SUM }'
@@ -14,11 +31,18 @@ mvn test | grep -o "Tests run: [0-9]" | sed 's/[^0-9]*//g' | awk '{ SUM += $1} E
 echo "[ Q ] How many commits do we have.."
 git rev-list --all --count
 
+echo "[ Q ] How many packages do we have.."
+grep -r "package" --include=*.java . --no-filename | sort -u | wc -l
+
 echo "[ Q ] How many lines of code do we have.."
 ( find ./ -name '*.java' -print0 | xargs -0 cat ) | wc -l
 
-echo ""
-echo "<<< MANUAL SUMMARY >>>"
-echo ""
+#[!] Manual output
+
 echo "[ Q ] How many design patterns do we have.."
 echo "1"
+
+#Used only when enhancing the script
+#TODO - remove once script is done
+cd scripts
+git checkout dev_sandor_minor_fixes --quiet
