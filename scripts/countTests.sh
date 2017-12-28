033f0eed3d6529c80c592864cb32630763e36037
Piotr Czyż <sabath91@gmail.com>
2017-12-28 23:38:31 +0100

    fix printing

--- scripts/countTests.sh
+++ scripts/countTests.sh
@@ -2,8 +2,8 @@
 
 mvn test | grep -A 2 Results | grep Tests | sed 's/[^0-9 ]//g' > tempFile.txt
 echo -n "Tests run: "; awk '{ SUM += $1} END { print SUM }' tempFile.txt
-echo -n ", Failures: "; awk '{ SUM += $2} END { print SUM }' tempFile.txt
-echo -n ", Errors: "; awk '{ SUM += $3} END { print SUM }' tempFile.txt
-echo -n ", Skipped: "; awk '{ SUM += $4} END { print SUM }' tempFile.txt
+echo -n "Failures: "; awk '{ SUM += $2} END { print SUM }' tempFile.txt
+echo -n "Errors: "; awk '{ SUM += $3} END { print SUM }' tempFile.txt
+echo -n "Skipped: "; awk '{ SUM += $4} END { print SUM }' tempFile.txt
 rm tempFile.txt
 
