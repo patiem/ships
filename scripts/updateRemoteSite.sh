916ec12906b69b5a008492bac9327e1b7f7da132
Sandor Korotkevics <sandor_korotkevics@epam.com>
2017-12-31 16:39:23 +0100

    Add a stub for updateRemoteSite.sh script

---
+++ scripts/updateRemoteSite.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Author: Sandor
+#
+# Since: 2017-12-31
+#
+# TODO: add extra info
+
+echo "Clonning repo [1].."
+echo "Clonning repo [2].."
+echo "Checking out to master branch on [1].."
+echo "Checking out to gh_pages branch on [2].."
+echo "Generating JaCoCo datafiles on [1].."
+echo "Generating site on [1].."
+echo "Staging site on [1].."
+echo "Removing all files on [2].."
+echo "Copying staged site from [1] to [2].."
+echo "Adding.. Committing.. Pushing (your credentials will be required).."
+echo "Done updating remote site. Cleaning up.."
+echo "Removing [1].."
+echo "Removing [2].."
+echo "Done cleaning."
