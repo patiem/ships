e2ed3a4ec7d82712a2f7cb94a3de2a20215a0e84
Sandor Korotkevics <sandor_korotkevics@epam.com>
2017-12-31 17:27:28 +0100

    Make updateRemoteSite.sh operate on a directory outside of current git repo

--- scripts/updateRemoteSite.sh
+++ scripts/updateRemoteSite.sh
@@ -6,17 +6,19 @@
 # Since: 2017-12-31
 #
 # TODO: add extra info
+#
 
+mkdir ../../tempUpdateRemoteSite
 echo "Clonning first repo [from].."
-./clone.sh from
+./clone.sh ../../tempUpdateRemoteSite/from
 echo "Clonning repo [to].."
-./clone.sh to
+./clone.sh ../../tempUpdateRemoteSite/to
 echo "Checking out to master branch on [from].."
-cd from
-git checkout master --quiet
+cd ../../tempUpdateRemoteSite/from
+git checkout master -q
 echo "Checking out to gh_pages branch on [to].."
 cd ../to
-git checkout gh-pages --quiet
+git checkout gh-pages -q
 echo "Generating JaCoCo datafiles on [from].."
 cd ../from
 mvn clean install -q
@@ -28,8 +30,12 @@ echo "Removing all files on [to].."
 cd ../to
 rm -rf *
 echo "Copying staged site from [from] to [to].."
+cd ..
+cp from/target/staging/. to/. -r
 echo "Adding.. Committing.. Pushing (your credentials will be required).."
-git add . -q; git commit -m 'Trigger site update' -q #; git push origin gh-pages
+git add . &
+git commit -m 'Trigger site update' -q
+#git push origin gh-pages
 echo "Done updating remote site. Cleaning up.."
 cd ..
 echo "Removing [from].."
