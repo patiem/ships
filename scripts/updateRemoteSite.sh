2e19600414401b7a337fd37006fc5a94f23a2eff
Sandor Korotkevics <sandor_korotkevics@epam.com>
2017-12-31 16:56:49 +0100

    Fill the script stub with some draft commands

--- scripts/updateRemoteSite.sh
+++ scripts/updateRemoteSite.sh
@@ -7,17 +7,33 @@
 #
 # TODO: add extra info
 
-echo "Clonning repo [1].."
-echo "Clonning repo [2].."
-echo "Checking out to master branch on [1].."
-echo "Checking out to gh_pages branch on [2].."
-echo "Generating JaCoCo datafiles on [1].."
-echo "Generating site on [1].."
-echo "Staging site on [1].."
-echo "Removing all files on [2].."
-echo "Copying staged site from [1] to [2].."
+echo "Clonning first repo [from].."
+./clone.sh from
+echo "Clonning repo [to].."
+./clone.sh to
+echo "Checking out to master branch on [from].."
+cd from
+git checkout master --quiet
+echo "Checking out to gh_pages branch on [to].."
+cd ../to
+git checkout gh-pages --quiet
+echo "Generating JaCoCo datafiles on [from].."
+cd ../from
+mvn clean install -q
+echo "Generating site on [from].."
+mvn site -q
+echo "Staging site on [from].."
+mvn site:stage -q
+echo "Removing all files on [to].."
+cd ../to
+rm -rf *
+echo "Copying staged site from [from] to [to].."
 echo "Adding.. Committing.. Pushing (your credentials will be required).."
+git add . -q; git commit -m 'Trigger site update' -q #; git push origin gh-pages
 echo "Done updating remote site. Cleaning up.."
-echo "Removing [1].."
-echo "Removing [2].."
+cd ..
+echo "Removing [from].."
+rm -rf from
+echo "Removing [to].."
+rm -rf to
 echo "Done cleaning."
