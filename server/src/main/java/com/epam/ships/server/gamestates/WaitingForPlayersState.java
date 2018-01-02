31d51570a2e99babd5999e3ee0946f4491cc48ee
Piotr Czyż <sabath91@gmail.com>
2018-01-02 20:58:02 +0100

    IOException handled at it root

--- server/src/main/java/com/epam/ships/server/gamestates/WaitingForPlayersState.java
+++ server/src/main/java/com/epam/ships/server/gamestates/WaitingForPlayersState.java
@@ -6,20 +6,17 @@ import com.epam.ships.infra.logging.core.SharedLogger;
 import com.epam.ships.server.CommunicationBus;
 import com.epam.ships.server.MessageSender;
 
-import java.io.IOException;
-
 public class WaitingForPlayersState implements GameState {
   private final Target logger = new SharedLogger(WaitingForPlayersState.class);
+  private CommunicationBus communicationBus;
+
+  public WaitingForPlayersState(CommunicationBus communicationBus) {
+    this.communicationBus = communicationBus;
+  }
 
   @Override
   public GameState process() {
-    CommunicationBus communicationBus = null;
-    try {
-      communicationBus = new CommunicationBus();
-      communicationBus.start();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    communicationBus.start();
 
     MessageSender messageSender = new MessageSender(communicationBus, logger);
     messageSender.sendToAll(Header.OPPONENT_CONNECTED);
