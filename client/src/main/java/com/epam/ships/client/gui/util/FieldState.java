8093b1a8414d9701af5d7394c2aa88ca8c750961
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-01-02 10:54:05 +0100

    add more javadocs

--- client/src/main/java/com/epam/ships/client/gui/util/FieldState.java
+++ client/src/main/java/com/epam/ships/client/gui/util/FieldState.java
@@ -1,5 +1,8 @@
 package com.epam.ships.client.gui.util;
 
+/**
+ * Enum represents state of board field.
+ */
 public enum FieldState {
   EMPTY,
   OCCUPIED
--- client/src/main/java/com/epam/ships/client/gui/util/ShipOrientation.java
+++ client/src/main/java/com/epam/ships/client/gui/util/ShipOrientation.java
@@ -1,5 +1,8 @@
 package com.epam.ships.client.gui.util;
 
+/**
+ * Enum to represent ship orientation.
+ */
 public enum ShipOrientation {
   HORIZONTAL,
   VERTICAL
