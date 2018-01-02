809a36ea083079d3b8f08d04ab1b2aaa8ee1e603
Piotr Czyż <sabath91@gmail.com>
2018-01-02 16:06:27 +0100

    Test for game using machine state in progress

---
+++ server/src/test/java/com/epam/ships/server/GameTest.java
@@ -0,0 +1,33 @@
+package com.epam.ships.server;
+
+import com.epam.ships.server.gamestates.GameState;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+@Test
+public class GameTest {
+
+  public void shouldEndAfterThreeCycles() {
+    //given
+
+    GameState initState = mock(GameState.class);
+    GameState secondState = mock(GameState.class);
+    GameState thirdState = mock(GameState.class);
+
+    when(initState.process()).thenReturn(secondState);
+    when(secondState.process()).thenReturn(thirdState);
+
+    when(initState.shouldBeContinued()).thenReturn(true);
+    when(secondState.shouldBeContinued()).thenReturn(true);
+    when(thirdState.shouldBeContinued()).thenReturn(false);
+
+
+    //when
+    new Game(initState).loop();
+
+    //then
+    verify(initState);
+  }
+
+}
