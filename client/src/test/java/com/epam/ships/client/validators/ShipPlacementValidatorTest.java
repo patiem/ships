73a24187ea779979ff5306e2c15a17fa8b688452
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-01-03 20:25:18 +0100

    first tests

--- client/src/test/java/com/epam/ships/client/validators/ShipPlacementValidatorTest.java
+++ client/src/test/java/com/epam/ships/client/validators/ShipPlacementValidatorTest.java
@@ -2,25 +2,31 @@ package com.epam.ships.client.validators;
 
 import com.epam.ships.client.gui.util.FieldState;
 import com.epam.ships.client.gui.util.ShipOrientation;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class ShipPlacementValidatorTest {
 
+  private static final int BOARD_SIZE = 10;
+
   @DataProvider(name = "validPlacement")
   public static Object [][] validData() {
     return new Object[][] {
-        {//orientation, // shipStartIndex, //mastCount, // board
-        }
+        {ShipOrientation.VERTICAL, 0, 3, producedBoard(Arrays.asList(99,81))}
     };
   }
 
   @DataProvider(name = "invalidPlacement")
   public static Object [][] invalidData() {
     return new Object[][] {
-        {}
+        {ShipOrientation.HORIZONTAL, 0, 3, producedBoard(Arrays.asList(0,1,2,3))}
     };
   }
 
@@ -50,4 +56,16 @@ public class ShipPlacementValidatorTest {
     assertFalse(isValid);
   }
 
+  private static FieldState [] producedBoard(List<Integer> occupiedPlaces) {
+    FieldState [] board = new FieldState[BOARD_SIZE * BOARD_SIZE];
+    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+      if(occupiedPlaces.contains(Integer.valueOf(i))) {
+        board[i] = FieldState.OCCUPIED;
+      } else {
+        board[i] = FieldState.EMPTY;
+      }
+    }
+
+    return board;
+  }
 }
