a8bf1a2a7c1017aa67694e00ed57a015688ec108
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-01-04 13:27:37 +0100

    help to first script

--- scripts/cloneAndAddHook.sh
+++ scripts/cloneAndAddHook.sh
@@ -14,10 +14,37 @@
 # * It is dependent on clone.sh script
 # * It is dependent on pre-commitHookMaven.sh script
 #
+function helpEcho {
+echo "[How to use]"
+echo " 1) Copy a script (if not willing to alias it) into a directory"
+echo
+echo "[Important]"
+echo "* It is dependent on clone.sh script"
+echo "* It is dependent on pre-commitHookMaven.sh script"
+echo
+echo "[Usage]"
+echo -e "\t ./cloneAndHook.sh [your_dir_name]"
+echo -e "\t\t [your_dir_name] - dir to which you want to clone"
+}
 
-./clone.sh $1
+if [[ ${#1} = 0 ]]; 
+	then echo -e "no args provided! \n\n" 
+	helpEcho
+	exit 1;
+fi
+if ! ./clone.sh $1;
+	then helpEcho
+	exit 1;
+fi
 cd $1
 echo "copying pre-commit hook"
-chmod u+x scripts/pre-commitHookMaven.sh
-cp scripts/pre-commitHookMaven.sh .git/hooks/pre-commit
+if ! chmod u+x scripts/pre-commitHookMaven.sh;
+	then helpEcho
+	exit 1;
+fi
+if ! cp scripts/pre-commitHookMaven.sh .git/hooks/pre-commit;
+	then helpEcho
+	exit 1;
+fi
 echo "hook added"
+
