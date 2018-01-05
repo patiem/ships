a80405e7ecd9e8b739f49c4e2275467cb732e99a
Sandor Korotkevics <sandor_korotkevics@epam.com>
2018-01-05 15:03:12 +0100

    Enhance Header, Author and Status enums

--- shared/src/main/java/pl/korotkevics/ships/shared/infra/communication/api/message/Header.java
+++ shared/src/main/java/pl/korotkevics/ships/shared/infra/communication/api/message/Header.java
@@ -1,7 +1,24 @@
 package pl.korotkevics.ships.shared.infra.communication.api.message;
 
+/**
+ * @author Sandor Korotkevics
+ * @since 2017-12-22
+ *
+ * It represents a message header defining
+ * a message content (main subject/what a
+ * message has inside).
+ */
+
 public enum Header {
-  DEFAULT, OPPONENT_CONNECTED, CONNECTION,
-  YOUR_TURN, MISS, HIT, SHIP_DESTRUCTED,
-  LOSE, WIN, SHOT, PLACEMENT
+  DEFAULT,
+  OPPONENT_CONNECTED,
+  CONNECTION,
+  YOUR_TURN,
+  MISS,
+  HIT,
+  SHIP_DESTRUCTED,
+  LOSE,
+  WIN,
+  SHOT,
+  PLACEMENT
 }
--- shared/src/main/java/pl/korotkevics/ships/shared/infra/communication/api/message/Status.java
+++ shared/src/main/java/pl/korotkevics/ships/shared/infra/communication/api/message/Status.java
@@ -1,5 +1,15 @@
 package pl.korotkevics.ships.shared.infra.communication.api.message;
 
+/**
+ * @author Sandor Korotkevics
+ * @since 2017-12-22
+ *
+ * It represents a message status:
+ * - OK if it is a regular message.
+ * - END if it is a last message in a batch.
+ */
+
 public enum Status {
-  OK, ERROR, END
+  OK,
+  END
 }
