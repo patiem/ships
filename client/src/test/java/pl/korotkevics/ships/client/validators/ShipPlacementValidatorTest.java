caf0ec9215a7e7fdd6fe6d09adfd87ad487c2dbd
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-01-14 18:19:52 +0100

    changes after CR

--- client/src/test/java/pl/korotkevics/ships/client/validators/ShipPlacementValidatorTest.java
+++ client/src/test/java/pl/korotkevics/ships/client/validators/ShipPlacementValidatorTest.java
@@ -18,35 +18,35 @@ public class ShipPlacementValidatorTest {
   @DataProvider(name = "validPlacement")
   public static Object [][] validData() {
     return new Object[][] {
-        {ShipOrientation.VERTICAL, 0, 3, producedBoard(Arrays.asList(99,81))},
-        {ShipOrientation.HORIZONTAL, 0, 3, producedBoard(Arrays.asList(4,2,3))},
-        {ShipOrientation.VERTICAL, 0, 4, producedBoard(Arrays.asList(20,30,40))},
-        {ShipOrientation.VERTICAL, 99, 1, producedBoard(Arrays.asList(20,30,40))},
-        {ShipOrientation.HORIZONTAL, 99, 1, producedBoard(Arrays.asList(20,30,40))},
-        {ShipOrientation.HORIZONTAL, 79, 1, producedBoard(Arrays.asList(80,90,70,60))},
-        {ShipOrientation.VERTICAL, 79, 1, producedBoard(Arrays.asList(80,90,70,60))}
+        {ShipOrientation.VERTICAL, 0, 3, produceBoard(Arrays.asList(99,81))},
+        {ShipOrientation.HORIZONTAL, 0, 3, produceBoard(Arrays.asList(4,2,3))},
+        {ShipOrientation.VERTICAL, 0, 4, produceBoard(Arrays.asList(20,30,40))},
+        {ShipOrientation.VERTICAL, 99, 1, produceBoard(Arrays.asList(20,30,40))},
+        {ShipOrientation.HORIZONTAL, 99, 1, produceBoard(Arrays.asList(20,30,40))},
+        {ShipOrientation.HORIZONTAL, 79, 1, produceBoard(Arrays.asList(80,90,70,60))},
+        {ShipOrientation.VERTICAL, 79, 1, produceBoard(Arrays.asList(80,90,70,60))}
     };
   }
 
   @DataProvider(name = "invalidPlacement")
   public static Object [][] invalidData() {
     return new Object[][] {
-        {ShipOrientation.HORIZONTAL, 0, 3, producedBoard(Arrays.asList(0,1,2,3))},
-        {ShipOrientation.VERTICAL, 0, 3, producedBoard(Arrays.asList(0,1,2,3))},
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(45,1,2,3))},
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(15,1,2,3))},
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(24,1,2,3))},
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(27,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 0, 3, produceBoard(Arrays.asList(0,1,2,3))},
+        {ShipOrientation.VERTICAL, 0, 3, produceBoard(Arrays.asList(0,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(45,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(15,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(24,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(27,1,2,3))},
         //corners
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(14,1,2,3))},
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(34,1,2,3))},
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(17,1,2,3))},
-        {ShipOrientation.VERTICAL, 25, 2, producedBoard(Arrays.asList(37,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(14,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(34,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(17,1,2,3))},
+        {ShipOrientation.VERTICAL, 25, 2, produceBoard(Arrays.asList(37,1,2,3))},
 
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(44,1,2,3))},
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(46,1,2,3))},
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(14,1,2,3))},
-        {ShipOrientation.HORIZONTAL, 25, 2, producedBoard(Arrays.asList(16,1,2,3))}
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(44,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(46,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(14,1,2,3))},
+        {ShipOrientation.HORIZONTAL, 25, 2, produceBoard(Arrays.asList(16,1,2,3))}
     };
   }
 
@@ -82,7 +82,7 @@ public class ShipPlacementValidatorTest {
     ShipOrientation shipOrientation = ShipOrientation.VERTICAL;
     int shipStartingIndex = 7;
     int mastCount = 4;
-    FieldState[] board = producedBoard(Arrays.asList(1));
+    FieldState[] board = produceBoard(Arrays.asList(1));
     ShipPlacementValidator shipPlacementValidator =
         new ShipPlacementValidator(shipOrientation, shipStartingIndex, mastCount, board);
 
@@ -99,7 +99,7 @@ public class ShipPlacementValidatorTest {
     ShipOrientation shipOrientation = ShipOrientation.VERTICAL;
     int shipStartingIndex = 6;
     int mastCount = 4;
-    FieldState[] board = producedBoard(Arrays.asList(1));
+    FieldState[] board = produceBoard(Arrays.asList(1));
     ShipPlacementValidator shipPlacementValidator =
         new ShipPlacementValidator(shipOrientation, shipStartingIndex, mastCount, board);
 
@@ -110,7 +110,7 @@ public class ShipPlacementValidatorTest {
     assertTrue(isValid);
   }
 
-  private static FieldState [] producedBoard(List<Integer> occupiedPlaces) {
+  private static FieldState [] produceBoard(final List<Integer> occupiedPlaces) {
     FieldState [] board = new FieldState[BOARD_SIZE * BOARD_SIZE];
     for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
       if(occupiedPlaces.contains(Integer.valueOf(i))) {
