2fd135481e53587013b78e82f20ccd6882e9fdc4
Sandor Korotkevics <sandor_korotkevics@epam.com>
2018-01-16 13:14:42 +0100

    Implement bilingual Connect button (it only works for Connect window)

--- client/src/main/java/pl/korotkevics/ships/client/gui/controllers/MainController.java
+++ client/src/main/java/pl/korotkevics/ships/client/gui/controllers/MainController.java
@@ -1,18 +1,18 @@
 package pl.korotkevics.ships.client.gui.controllers;
 
-import javafx.scene.image.ImageView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import lombok.Getter;
 import pl.korotkevics.ships.client.client.Client;
 import pl.korotkevics.ships.client.localization.Locale;
 import pl.korotkevics.ships.shared.infra.logging.api.Target;
 import pl.korotkevics.ships.shared.infra.logging.core.SharedLogger;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.Pane;
-import lombok.Getter;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 /**
@@ -24,46 +24,58 @@ import java.util.ResourceBundle;
 public class MainController {
 
   private static final Target logger = new SharedLogger(Client.class);
-
+  
   @FXML
   private Pane mainPane;
 
   @Getter
   private Client client;
 
-  @FXML
-  private ImageView imEnglandFlag;
-
-  @FXML
-  private ImageView imPolandFlag;
-
   private ResourceBundle resourceBundle;
-
+  
+  @FXML
+  Button polishButton;
+  
+  @FXML
+  Button englishButton;
+  
   /**
    * Initialize main controller.
    * @param client - Instance of Client, which communicate with server.
    */
   @FXML
   public void initialize(final Client client) {
-    final String connectWindowUrl = "/fxml/connectWindow.fxml";
     this.client = client;
-
-    this.resourceBundle = ResourceBundle.getBundle(Locale.ENGLISH.toString());
-
-    this.imEnglandFlag.setOnMouseClicked(e -> this.resourceBundle = ResourceBundle.getBundle(Locale.ENGLISH.toString()));
-    this.imPolandFlag.setOnMouseClicked(e -> this.resourceBundle = ResourceBundle.getBundle(Locale.POLISH.toString()));
-
+    this.prepareLocalizationButtons();
+    this.loadView(Locale.ENGLISH);
+  }
+  
+  @FXML
+  void triggerPolishVersion(ActionEvent event) {
+    this.loadView(Locale.POLISH);
+  }
+  
+  @FXML
+  void triggerEnglishVersion(ActionEvent event) {
+    this.loadView(Locale.ENGLISH);
+  }
+  
+  private void loadView(Locale locale) {
+    final String connectWindowUrl = "/fxml/connectWindow.fxml";
+    final FXMLLoader connectLoader = new FXMLLoader(getClass().getResource(connectWindowUrl));
+    this.resourceBundle = ResourceBundle.getBundle(locale.toString());
+    connectLoader.setResources(this.resourceBundle);
     try {
-      final FXMLLoader connectLoader = new FXMLLoader(getClass().getResource(connectWindowUrl));
-      connectLoader.setResources(this.resourceBundle);
       final Parent connect = connectLoader.load();
-
       mainPane.getChildren().clear();
       mainPane.getChildren().add(connect);
     } catch (IOException e) {
       logger.error(e.getMessage());
     }
   }
-
-
+  
+  private void prepareLocalizationButtons() {
+    this.englishButton.setOnMouseClicked(e -> this.resourceBundle = ResourceBundle.getBundle(Locale.ENGLISH.toString()));
+    this.polishButton.setOnMouseClicked(e -> this.resourceBundle = ResourceBundle.getBundle(Locale.POLISH.toString()));
+  }
 }
