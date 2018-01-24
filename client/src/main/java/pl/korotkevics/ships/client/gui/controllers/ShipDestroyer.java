8f6386834751f2d5dadc652718b029df354b6bef
Magdalena Aarsman <magdalena_aarsman@epam.com>
2018-01-24 13:40:55 +0100

    sinking vertical ships

--- client/src/main/java/pl/korotkevics/ships/client/gui/controllers/ShipDestroyer.java
+++ client/src/main/java/pl/korotkevics/ships/client/gui/controllers/ShipDestroyer.java
@@ -19,46 +19,108 @@ public class ShipDestroyer {
   }
 
   public Set<Integer> getIndexToColor() {
-    final int maxMastCount = 4;
-
-    if(isOneMastShip()) {
-      return indexexToSetEmpty;
+    if(this.isOneMastShip()) {
+      return this.indexexToSetEmpty;
     }
 
-    return indexexToSetEmpty;
+    if(this.shipOrientation.equals(ShipOrientation.VERTICAL)) {
+      this.sinkVertical();
+    } else {
+      this.sinkHorizontal();
+    }
+
+    return this.indexexToSetEmpty;
+  }
+
+  private void sinkHorizontal() {
+    //while ()
+  }
+
+  private void sinkVertical() {
+    //check down
+    int index = this.lastHitIndex;
+    while ((index < this.board.length )
+    && index % BOARD_SIZE != 9 && this.board[index].equals(FieldState.HIT)) {
+
+      if(index - BOARD_SIZE > 0) {
+        this.indexexToSetEmpty.add(index - BOARD_SIZE);
+      }
+
+      if(index + BOARD_SIZE > 0) {
+        this.indexexToSetEmpty.add(index + BOARD_SIZE);
+      }
+      index++;
+    }
+
+    if(index < this.board.length && this.board[index].equals(FieldState.EMPTY)) {
+      this.indexexToSetEmpty.add(index);
+    }
+
+    if(index - BOARD_SIZE > 0) {
+      this.indexexToSetEmpty.add(index - BOARD_SIZE);
+    }
+
+    if(index + BOARD_SIZE < this.board.length) {
+      this.indexexToSetEmpty.add(index + BOARD_SIZE);
+    }
+
+    //check up
+    index = this.lastHitIndex;
+    while ((index > 0 )
+    && index % BOARD_SIZE != 0 && this.board[index].equals(FieldState.HIT)) {
+      if(index - BOARD_SIZE >= 0) {
+        this.indexexToSetEmpty.add(index - BOARD_SIZE);
+      }
+
+      if(index + BOARD_SIZE < board.length) {
+        this.indexexToSetEmpty.add(index + BOARD_SIZE);
+      }
+      index--;
+    }
+    if(index > 0 && this.board[index].equals(FieldState.EMPTY)) {
+      this.indexexToSetEmpty.add(index);
+    }
+
+    if(index - BOARD_SIZE >= 0) {
+      this.indexexToSetEmpty.add(index - BOARD_SIZE);
+    }
+
+    if(index + BOARD_SIZE < this.board.length) {
+      this.indexexToSetEmpty.add(index + BOARD_SIZE);
+    }
   }
 
   private boolean isOneMastShip() {
 
-    final int startCheckIndex = lastHitIndex % 10 == 0 ? lastHitIndex : lastHitIndex - 1;
-    final int endCheckIndex = lastHitIndex % 9 == 0 ? lastHitIndex : lastHitIndex + 1;
+    final int startCheckIndex = lastHitIndex % BOARD_SIZE == 0 ? lastHitIndex : lastHitIndex - 1;
+    final int endCheckIndex = lastHitIndex % BOARD_SIZE == 9 ? lastHitIndex : lastHitIndex + 1;
 
     for(int i = startCheckIndex; i <= endCheckIndex; i++) {
 
       //check only right/left
-      if(i - BOARD_SIZE > 0 ) {
-        if(board[i - BOARD_SIZE].equals(FieldState.EMPTY)) {
-          indexexToSetEmpty.add(i - BOARD_SIZE);
+      if(i - BOARD_SIZE >= 0 ) {
+        if(this.board[i - BOARD_SIZE].equals(FieldState.EMPTY)) {
+          this.indexexToSetEmpty.add(i - BOARD_SIZE);
         } else {
-          shipOrientation = ShipOrientation.HORIZONTAL;
+          this.shipOrientation = ShipOrientation.HORIZONTAL;
           return false;
         }
       }
 
-      if(i + BOARD_SIZE < board.length ) {
-        if(board[i + BOARD_SIZE].equals(FieldState.EMPTY)) {
-          indexexToSetEmpty.add(i + BOARD_SIZE);
+      if(i + BOARD_SIZE < this.board.length ) {
+        if(this.board[i + BOARD_SIZE].equals(FieldState.EMPTY)) {
+          this.indexexToSetEmpty.add(i + BOARD_SIZE);
         } else {
-          shipOrientation = ShipOrientation.HORIZONTAL;
+          this.shipOrientation = ShipOrientation.HORIZONTAL;
           return false;
         }
       }
 
-      if(i != lastHitIndex) {
-        if(board[i].equals(FieldState.EMPTY)) {
-          indexexToSetEmpty.add(i);
+      if(i != this.lastHitIndex) {
+        if(this.board[i].equals(FieldState.EMPTY)) {
+          this.indexexToSetEmpty.add(i);
         } else {
-          shipOrientation = ShipOrientation.VERTICAL;
+          this.shipOrientation = ShipOrientation.VERTICAL;
           return false;
         }
       }
